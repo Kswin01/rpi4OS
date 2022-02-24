d2ec782d65b6e960a0c80fd6f5357d05ca451720
Krishnan <krishnanwinter1@gmail.com>
2022-02-24 17:28:20 +1100

    Added header guard

--- include/string.h
+++ include/string.h
@@ -1,3 +1,6 @@
+#ifndef STRING_H_  
+#define STRING_H_
+
 //strcpy, strcat, memcpy, memset
 
 char *strcpy(char *dest, const char *src);
@@ -7,3 +10,5 @@ char *strcat(char *dest, const char *src);
 void *memcpy(void *dest, const void *src, size_t n);
 
 void *memset(void *s, int c, size_t n);
+
+#endif

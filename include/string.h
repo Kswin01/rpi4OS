698539ffeedf55c0e076662235a1d69cbad05e01
Krishnan <krishnanwinter1@gmail.com>
2022-02-24 17:24:51 +1100

    not included in last commit, added function prototypes to implement in string.h and added in printf test to kernel

---
+++ include/string.h
@@ -0,0 +1,9 @@
+//strcpy, strcat, memcpy, memset
+
+char *strcpy(char *dest, const char *src);
+
+char *strcat(char *dest, const char *src);
+
+void *memcpy(void *dest, const void *src, size_t n);
+
+void *memset(void *s, int c, size_t n);
--- kernel.c
+++ kernel.c
@@ -1,8 +1,11 @@
 #include "io.h"
+#include "include/stdio.h"
 
 void main()
 {
     uart_init();
     uart_writeText("Hello world!\n");
+    printf("Hello, does this printf work?\n");
+
     while (1);
 }
